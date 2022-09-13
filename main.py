d17e983cc2853d910221e64bf7333d619acf2f6c
Bourassa <BBOURASSA@student.bridgew.edu>
2022-09-12 20:32:19 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+message = "hello"
+message1 = "goodbye"
+print (message)
+print (message1)
+#yourname = input ("what is your name:")
+#print ("wow", yourname, "is a really cool name" )
+#yourname = input ("what is your name?")
+#yourmajor = input ("and what is your major?")
+#print ("thats really cool!", yourname,"i heard that comp151 really helps understanding", yourmajor   )
+message3 = 'that boy said "how are you doing" to me'
+print (message3)
+comment_to_user = f'he said his name was "{message}" yesterday'
+print (comment_to_user)
